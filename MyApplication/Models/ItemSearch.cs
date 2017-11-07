c8b92eb3967f847d23051ac570ee570ba0fd90f5
billy bennett <billybennett@outlook.com>
2017-11-07 16:14:27 +0000

    Second Commit 3 classes added to the models section

---
+++ MyApplication/Models/ItemSearch.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyApplication.Models
+{
+    public class ItemSearch
+    {
+        public string SearchItemName { get; set; }
+        public string SearchFilter { get; set; }
+        public string SearchItemInformation { get; set; }
+        public string SearchItemAvailability { get; set; }
+       
+}
---
+++ MyApplication/Models/UserRegisteration.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyApplication.Models
+{
+    public class UserRegisteration
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string EmailAddress { get; set; }
+        public string Postcode { get; set; }
+        public string MobileNum { get; set; }
+        public string ProfileImg { get; set; } ///String of the filepath of img file
+    }
+}
