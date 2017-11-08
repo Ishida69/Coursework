50c209321e48780c48de4376ce324dbe1ffaff28
billy bennett <billybennett@outlook.com>
2017-11-08 07:58:41 +0000

    Re-commit not all the files where correctly commited

--- MyApplication/Models/ListingItems.cs
+++ MyApplication/Models/ListingItems.cs
@@ -5,7 +5,15 @@ using System.Web;
 
 namespace MyApplication.Models
 {
-    public class Class1
+    public class ListingItems
     {
+        public string ItemImg { get; set; } //string of the filepath for the image
+        public string ItemTitle { get; set; }
+        public string ItemDescription { get; set; }
+        public double Price { get; set; }
+        public string ItemCondition { get; set; }
+        public string Category { get; set; }
+ 
+
     }
 }
